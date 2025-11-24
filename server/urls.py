bd1639e1ed706451a3e825b702014e916ca0f2f1
Rezvon1409 <rezvon0880@gmail.com>
2025-11-24 14:47:21 +0500

    create model , serializer.py , urls.py and write logic in view.py file in accounts app

--- server/urls.py
+++ server/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
-    # path('api/restaurant/', include('restaurant.urls')),
+    path('api/restaurant/', include('restaurant.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
+
 ]
