0009f18d207c03dfda8b3180814051a314256394
Rezvon1409 <rezvon0880@gmail.com>
2025-11-24 16:03:47 +0500

    create view.py and urls.py

---
+++ restaurant/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from .views import (
+    RestaurantListCreateAPIView, RestaurantDetailAPIView,
+    DeliveryZoneListCreateAPIView, DeliveryZoneDetailAPIView,
+    CategoryListCreateAPIView, CategoryDetailAPIView,
+    FoodListCreateAPIView, FoodDetailAPIView,
+    AddOnListCreateAPIView, AddOnDetailAPIView,
+    PromocodeListCreateAPIView, PromocodeDetailAPIView,
+    OrderListCreateAPIView, OrderDetailAPIView,
+    OrderItemListCreateAPIView, OrderItemDetailAPIView,
+    OrderItemAddOnListCreateAPIView, OrderItemAddOnDetailAPIView
+)
+
+urlpatterns = [
+    path("restaurants/", RestaurantListCreateAPIView.as_view(), name="restaurant-list"),
+    path("restaurants/<int:pk>/", RestaurantDetailAPIView.as_view(), name="restaurant-detail"),
+    path("zones/", DeliveryZoneListCreateAPIView.as_view(), name="zone-list"),
+    path("zones/<int:pk>/", DeliveryZoneDetailAPIView.as_view(), name="zone-detail"),
+    path("categories/", CategoryListCreateAPIView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetailAPIView.as_view(), name="category-detail"),
+    path("foods/", FoodListCreateAPIView.as_view(), name="food-list"),
+    path("foods/<int:pk>/", FoodDetailAPIView.as_view(), name="food-detail"),
+    path("addons/", AddOnListCreateAPIView.as_view(), name="addon-list"),
+    path("addons/<int:pk>/", AddOnDetailAPIView.as_view(), name="addon-detail"),
+    path("promocodes/", PromocodeListCreateAPIView.as_view(), name="promocode-list"),
+    path("promocodes/<int:pk>/", PromocodeDetailAPIView.as_view(), name="promocode-detail"),
+    path("orders/", OrderListCreateAPIView.as_view(), name="order-list"),
+    path("orders/<int:pk>/", OrderDetailAPIView.as_view(), name="order-detail"),
+    path("order-items/", OrderItemListCreateAPIView.as_view(), name="orderitem-list"),
+    path("order-items/<int:pk>/", OrderItemDetailAPIView.as_view(), name="orderitem-detail"),
+    path("order-item-addons/", OrderItemAddOnListCreateAPIView.as_view(), name="orderitemaddon-list"),
+    path("order-item-addons/<int:pk>/", OrderItemAddOnDetailAPIView.as_view(), name="orderitemaddon-detail"),
+]
